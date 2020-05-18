b32c34594dff09ff8dca23a8e5226297197e32ab
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-05-17 19:21:26 -0700

    background purple

--- convergence/footer.php
+++ convergence/footer.php
@@ -11,22 +11,13 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'convergence' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'convergence' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'convergence' ), 'convergence', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer>
+		<div>
+			&copy;
+			<?php echo date('Y'); ?>
+			Convergence Design Lab, LLC
+		</div>
+	</footer>
 
 <?php wp_footer(); ?>
 
