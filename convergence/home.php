da037d6bfd4fc01ae81ef92146fe5330beac3cb2
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-05-26 14:49:27 -0700

    wrap the button in a link that goes to a ACF page

--- convergence/home.php
+++ convergence/home.php
@@ -11,6 +11,16 @@
     <section class="firstSection">
         <h1><?php echo the_field('tagline');?></h1>
 		<div class="taglineContainer">
+                <a href=<?php echo the_field('button_one_link');?> target="_blank">
+                    <div class="button">
+                        <h4><?php echo the_field('button_one_title');?></h4>
+                    </div>
+                </a>
+                <a href=<?php echo the_field('button_two_link');?> target="_blank">
+                    <div class="button">
+                        <h4><?php echo the_field('button_two_title');?></h4>
+                    </div>
+                </a>
 		</div>
     </section>
 </main>
--- convergence/tools.php
+++ convergence/tools.php
@@ -11,6 +11,11 @@
 <section class="nonHomeSection">
 		<div class="taglineContainer">
 			<h2><?php wp_title( '' ); ?></h2>
+                <a href=<?php echo the_field('button_one_link');?> target="_blank">
+                    <div class="button">
+                        <h4><?php echo the_field('button_one_text');?></h4>
+                    </div>
+                </a>
 		</div>
     </section>
 </main>
