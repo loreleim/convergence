17ddfdce741dcceeb44d8392b5e48e2d2166bef3
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-06-01 23:02:21 -0700

    hamburger menu code

--- convergence/header.php
+++ convergence/header.php
@@ -40,3 +40,17 @@
 		</div>
 
 	</header>
+	
+	<script type="text/javascript">
+	document.getElementById("hammy").addEventListener('click', function() {
+		if (this.classList.contains("on")) {
+		this.classList.remove("on");
+		this.classList.add("off");
+	}
+	else {
+		this.classList.remove("off");
+		this.classList.add("on");
+	}
+													  });
+		
+    </script>
