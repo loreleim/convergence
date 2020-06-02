f1d2b959484eab7428d24873d1131b8931ef0009
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-06-01 22:51:25 -0700

    parent container for the services section

--- convergence/home.php
+++ convergence/home.php
@@ -42,6 +42,9 @@
 		</div>
 		<img src="<?php echo the_field('section2_image');?>"/>
 	</section>
+	
+	<section>
+		<div class="servicesSection">
 			<h5><?php echo the_field('section3_title');?></h5>
 			<div class="servicesTrio">
 					<div class="servicesIcon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283 182.6"><g data-name="Layer 2"><g data-name="Layer 1"><g data-name="Group 620"><path data-name="Path 952" d="M100 91a91 91 0 0142-76 91 91 0 100 152 91 91 0 01-42-76z" fill="#45f4ef"/><path data-name="Path 953" d="M192 0a91 91 0 00-50 15 91 91 0 010 152A91 91 0 10192 0z" fill="#ff5050"/></g><path d="M168 141a91 91 0 0015-47v-3-2a91 91 0 00-41-74 91 91 0 00-41 76v1a91 91 0 0041 75 91 91 0 0026-26z" fill="#23144c"/></g> </g> </svg></div>
